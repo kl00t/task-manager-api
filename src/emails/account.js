70d3526f927569f6c569a1729ca1b709aa730a6a
Scott Vaughan <scott.vaughan@magairports.com>
2020-09-23 19:04:20 +0100

    Update environment variable

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const to = process.env.SENDGRID_TO_ADDRESS
 const from = process.env.SENDGRID_FROM_ADDRESS
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
-        //to: email,
-        to,
+        to: email,
         from,
         subject: 'Welcome to the task manager app!',
         text: `Hello there ${name}. Let me know how you get along with the app.`
@@ -16,8 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
-        //to: email,
-        to,
+        to: email,
         from,
         subject: 'You have deleted your account!',
         text: `We are sorry to see you go ${name}. Is there anything we could have do to make you stay?`
--- src/models/user.js
+++ src/models/user.js
@@ -78,7 +78,7 @@ userSchema.methods.toJSON = function () {
 // Generate an auth token based on the user credentials.
 userSchema.methods.generateAuthToken = async function() {
     const user = this
-    const token = jwt.sign( { _id: user._id.toString() }, 'THIS_IS_A_SECRET')
+    const token = jwt.sign( { _id: user._id.toString() }, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
